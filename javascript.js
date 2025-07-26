21f2e3e5aee0e778fa1095b90c8b2202836c7012
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-26 10:43:45 +0800

    Write pseudocode.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-    console.log("Hello, World!")
+// Computer will return "rock", "paper", "scissors"
+    // Return 3 random numbers
+    // Assign "rock", "paper", or "scissors"
+        // If 0 assign "rock"
+        // If 1 assign "paper"
+        // If 2 assign "scissors"
+// Prompt user for choice 
+// Create two variables to track human and computer scores
+// Track scores for each round
+    // Take human and computer choice as arguments
+    // Increment round winner's score
+    // Return winner announcement
