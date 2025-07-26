feddccd824e0aac4b399e5175bcc350536893135
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-26 13:24:12 +0800

    Write logic to get the computer choice.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
 // Computer will return "rock", "paper", "scissors"
-    // Return 3 random numbers
+
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3); // Return 3 random numbers    
     // Assign "rock", "paper", or "scissors"
-        // If 0 assign "rock"
-        // If 1 assign "paper"
-        // If 2 assign "scissors"
+    if (computerChoice == 0) {  // If 0 assign "rock"
+        return "Rock";
+    } else if (computerChoice == 1) { // If 1 assign "paper"
+        return "Paper";
+    } else { // If 2 assign "scissors"
+        return "Scissors";
+    }
+}
+   
 // Prompt user for choice 
 // Create two variables to track human and computer scores
 // Track scores for each round
