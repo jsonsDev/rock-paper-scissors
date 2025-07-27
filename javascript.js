40486e3d6bc62f1600d8b655a9e3b41d9dd06481
jsonsDev <183668194+jsonsDev@users.noreply.github.com>
2025-07-28 02:23:21 +0800

    Remove comments left in code.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,24 @@
-// Computer will return "rock", "paper", "scissors"
 function getComputerChoice() {
-    const computerPlays = Math.floor(Math.random() * 3); // Return 3 random numbers    
-    // Assign "rock", "paper", or "scissors"
-    if (computerPlays == 0) {  // If 0 assign "rock"
+    const computerPlays = Math.floor(Math.random() * 3);     
+    if (computerPlays == 0) { 
         return "rock";
-    } else if (computerPlays == 1) { // If 1 assign "paper"
+    } else if (computerPlays == 1) { 
         return "paper";
-    } else { // If 2 assign "scissors"
+    } else { 
         return "scissors";
     }
 }
 
-// Prompt user for choice
 function getHumanChoice() {
     const humanPlays = prompt("Choose rock, paper, or scissors.");
     return humanPlays.toLowerCase();
 }
 
-// Track scores for each round
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-// Create function to play 5 rounds
 function playGame() {
 
-    // Create function to play a single round
     function playRound(humanChoice, computerChoice) {
         if (humanChoice == "rock" && computerChoice == "paper") {
             console.log("You lose! Paper beats rock!");
@@ -56,7 +50,6 @@ function playGame() {
     console.log("Current score:", "You:", humanScore, "Computer:", computerScore);
 }
 
-// Play 5 rounds by calling playRound 5 times
 for (i = 0; i < 5; i++) {
 playGame();
 }
